6ae0be5871346faa8328b7c37ebe044f53dc2205
Jhon Villamizar <jhonvio@hotmail.com>
2024-02-01 15:51:05 -0500

    add: manage of data with JSON

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { BookRoutes } from './interfaces/routes/book.routes';
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 app.use('/api', BookRoutes());
 
